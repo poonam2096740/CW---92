b94c188b674712a469f5c694073c995c0ebe8da2
poonam2096740 <85899531+poonam2096740@users.noreply.github.com>
2022-01-18 23:12:33 +0530

    Update game_login.js

--- game_login.js
+++ game_login.js
@@ -5,6 +5,7 @@ player_2_name = document.getElementById("player_2_name").value;
 localStorage.setItem("player_1_name", player_1_name);
 localStorage.setItem("player_2_name", player_2_name);
 
-window.location("game_page.html");
+window.location="game_page.html";
 }
+
 
